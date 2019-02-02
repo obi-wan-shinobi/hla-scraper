716b77eb6beed41104f1d2caf51b3d76fac4faf5
Kai Marshland <kaimarshland@gmail.com>
2019-02-01 17:24:14 -0800

    Made a tool to decolourize images

---
+++ decolourize_images.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const path = require('path');
+const sharp = require('sharp');
+
+function outputFile(inputFile) {
+    return inputFile.replace(/image-\d+/, (originalName) => {
+        return originalName + '-bw';
+    });
+}
+
+function decolourizeImage(imageFile) {
+    return sharp(imageFile)
+        .greyscale()
+        .toFile(outputFile(imageFile))
+        .catch((error) => {
+            console.log('Error in', imageFile);
+            throw error;
+        });
+}
+
+async function decolourizeDirectory(dir) {
+    const startTime = Date.now();
+
+    const files = await promisify(fs.readdir)(dir);
+
+    const coloured = new Set();
+    const bw = new Set();
+
+    for (const file of files) {
+        if (/image-\d+-bw\.jpeg/.test(file)) {
+            bw.add(file);
+        } else if (/image-\d+\.jpeg/.test(file)) {
+            coloured.add(file);
+        }
+    }
+
+    let count = 0;
+    for (const file of coloured) {
+        if (bw.has(outputFile(file))) {
+            continue;
+        }
+
+        await decolourizeImage(path.join(dir, file));
+        count++;
+    }
+
+    const elapsed = Date.now() - startTime;
+
+    console.log(`\t${count} decolourized in ${elapsed.toFixed()}ms (${dir} contains ${coloured.size} in colour)`);
+
+    return coloured.size;
+}
+
+async function decolourizeAll() {
+    const startTime = Date.now();
+
+    const objectDir = 'data/objects';
+    const files = await promisify(fs.readdir)(objectDir);
+
+    let totalCount = 0;
+
+    for (const file of files) {
+        const subdir = path.join(objectDir, file);
+        if (!(await promisify(fs.stat)(subdir)).isDirectory()) {
+            continue;
+        }
+
+        totalCount += await decolourizeDirectory(subdir);
+    }
+
+    const elapsed = Date.now() - startTime;
+    console.log(`Done in ${elapsed}ms (${totalCount} black and white images)`);
+}
+
+decolourizeAll();
