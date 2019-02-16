237b4f93774213dca00b04528be0dfccb9d4af29
Kai Marshland <kaimarshland@gmail.com>
2019-02-15 17:31:07 -0800

    Let it crop folders

--- resize_images.js
+++ resize_images.js
@@ -1,27 +1,32 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const path = require('path');
 const sharp = require('sharp');
+const mkdirp = promisify(require('mkdirp'));
 
 const WIDTH = 2040;
 const HEIGHT = 1356;
 
-async function resizeImage(imagePath, outputPath) {
+async function resizeImage(imagePath, outputPath, i) {
     const image = sharp(imagePath);
 
     // get image size
     const [width, height] = await getImageSize(image);
-    console.log(width, height);
 
     if (width < WIDTH || height < HEIGHT) {
-        return;
+        return i;
     }
 
-    let i = 0;
+    i = i || 0;
     for (let x = 0; x < width; x += WIDTH) {
-        console.log('x: ' + x);
         x = Math.min(x, width - WIDTH);
 
         for (let y = 0; y < height; y += HEIGHT) {
-            console.log('y: ' + y);
             y = Math.min(y, height - HEIGHT);
+            if (fs.existsSync(`${outputPath}-${i}.jpg`)) {
+                i++;
+                continue;
+            }
 
             await image.extract({
                 left: x,
@@ -32,7 +37,7 @@ async function resizeImage(imagePath, outputPath) {
         }
     }
 
-    return outputPath;
+    return i;
 }
 
 function getImageSize(image) {
@@ -42,4 +47,24 @@ function getImageSize(image) {
         });
 }
 
-resizeImage('data/heritage/image-0.jpeg', 'data/test').then(console.log);
+async function cropFolder(dir, outputDir) {
+    console.log(`Cropping ${dir}`);
+    await mkdirp(outputDir);
+
+    let files = await promisify(fs.readdir)(dir);
+    files = files.filter((file) => /\.(png|jpe?g)$/.test(file));
+    files = files.sort((a, b) => parseInt(a.match(/-(\d+)/)[1]) - parseInt(b.match(/-(\d+)/)[1]));
+
+    let i = 0;
+    for (const file of files) {
+        console.log(`\tCropping ${file}`);
+        const prevI = i;
+        i = await resizeImage(path.join(dir, file), `${outputDir}/image`, i)
+        console.log(`\t\t${i - prevI} cropped from ${file} (${i} done total)`);
+    }
+
+    return i;
+}
+
+// cropFolder('data/heritage', 'data/cropped-heritage').then(console.log);
+cropFolder('data/hubblesite', 'data/cropped-hubblesite').then(console.log);
